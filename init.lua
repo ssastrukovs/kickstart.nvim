b7775300c729ab32560f08258fc361f85c629e5b
Savva Strukov <ssa_strukovs@mail.ru>
2024-11-11 17:03:46 +0400

    add git hunk formatting and clangd types
    TBD bugs, maybe relocate to files

--- init.lua
+++ init.lua
@@ -577,6 +577,7 @@ require('lazy').setup({
             on_attach(client, bufnr)
           end,
           capabilities = capabilities,
+          filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'h', 'hpp', 'cxx', 'hxx', 'cc' },
         },
         basedpyright = {
           settings = {
@@ -665,9 +666,50 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, conf = true, icli = true }
+        -- Disable format_hunks() when in disable_filetypes
+        local disable_filetypes_formathunks = { 'lua', 'icli', 'conf' }
+        local function format_hunks()
+          local hunks = require('gitsigns').get_hunks()
+          if hunks == nil then
+            return
+          end
+
+          local format = require('conform').format
+
+          local function format_range()
+            if next(hunks) == nil then
+              vim.notify('done formatting git hunks', 'info', { title = 'formatting' })
+              return
+            end
+            local hunk = nil
+            while next(hunks) ~= nil and (hunk == nil or hunk.type == 'delete') do
+              hunk = table.remove(hunks)
+            end
+
+            if hunk ~= nil and hunk.type ~= 'delete' then
+              local start = hunk.added.start
+              local last = start + hunk.added.count
+              -- nvim_buf_get_lines uses zero-based indexing -> subtract from last
+              local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+              local range = { start = { start, 0 }, ['end'] = { last - 1, last_hunk_line:len() } }
+              format({ range = range, async = true, lsp_fallback = true }, function()
+                vim.defer_fn(function()
+                  format_range()
+                end, 1)
+              end)
+            end
+          end
+
+          format_range()
+        end
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
+          -- format only changed in disabled, a.k.a.
+          -- vscode "editor.formatOnSaveMode": "modificationsIfAvailable"
+          if not disable_filetypes_formathunks[vim.bo[bufnr].filetype] then
+            format_hunks()
+          end
           lsp_format_opt = 'never'
         else
           lsp_format_opt = 'fallback'
