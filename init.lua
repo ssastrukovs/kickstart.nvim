64edf4a250bf7a54e157c8498b21d350ab701307
Savva Strukov <ssa_strukovs@mail.ru>
2024-12-20 17:10:26 +0400

    remove kitty smears
    too slow, fine as is

--- init.lua
+++ init.lua
@@ -445,35 +445,35 @@ require('lazy').setup({
   --     end,
   --   },
   -- },
-  {
-    'sphamba/smear-cursor.nvim',
-    opts = {
-      -- carbonfox. Maybe get it from terminal config?
-      cursor_color = '#b6b8bb',
-      -- Smear cursor when switching buffers or windows.
-      smear_between_buffers = true,
-
-      -- Smear cursor when moving within line or to neighbor lines.
-      smear_between_neighbor_lines = true,
-
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears will blend better on all backgrounds.
-      -- legacy_computing_symbols_support = true,
-
-      -- quick opts
-      stiffness = 0.8, -- 0.6      [0, 1]
-      trailing_stiffness = 0.5, -- 0.3      [0, 1]
-      distance_stop_animating = 0.5, -- 0.1      > 0
-      hide_target_hack = false, -- true     boolean
-
-      -- FIRE OPTS
-      -- stiffness = 0.3,
-      -- trailing_stiffness = 0.1,
-      -- trailing_exponent = 3,
-      -- gamma = 1,
-      -- volume_reduction_exponent = -0.1,
-    },
-  },
+  -- {
+  --   'sphamba/smear-cursor.nvim',
+  --   opts = {
+  --     -- carbonfox. Maybe get it from terminal config?
+  --     cursor_color = '#b6b8bb',
+  --     -- Smear cursor when switching buffers or windows.
+  --     smear_between_buffers = true,
+  --
+  --     -- Smear cursor when moving within line or to neighbor lines.
+  --     smear_between_neighbor_lines = true,
+  --
+  --     -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+  --     -- Smears will blend better on all backgrounds.
+  --     -- legacy_computing_symbols_support = true,
+  --
+  --     -- quick opts
+  --     stiffness = 0.8, -- 0.6      [0, 1]
+  --     trailing_stiffness = 0.5, -- 0.3      [0, 1]
+  --     distance_stop_animating = 0.5, -- 0.1      > 0
+  --     hide_target_hack = false, -- true     boolean
+  --
+  --     -- FIRE OPTS
+  --     -- stiffness = 0.3,
+  --     -- trailing_stiffness = 0.1,
+  --     -- trailing_exponent = 3,
+  --     -- gamma = 1,
+  --     -- volume_reduction_exponent = -0.1,
+  --   },
+  -- },
 
   --diffview
   {
